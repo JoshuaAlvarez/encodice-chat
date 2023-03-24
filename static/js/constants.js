64691a55d33f650634815e35e4031519e217f9ac
JoshuaAlvarez <kornnlore@gmail.com>
2023-03-24 08:54:42 -0600

    Updated tunnel

--- static/js/constants.js
+++ static/js/constants.js
@@ -1,3 +1,3 @@
 const action_name = "utter_greet";
-const rasa_server_url = "https://811d-2806-2f0-54c0-2f52-603d-cdba-6b7b-20f3.ngrok.io/webhooks/rest/webhook";
+const rasa_server_url = "https://e504-2806-2f0-54c0-2f52-5c85-9e44-d1e0-2bd1.ngrok.io/webhooks/rest/webhook";
 const sender_id = uuidv4();
